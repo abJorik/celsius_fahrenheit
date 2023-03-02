d9297f2c8e9530f68dbf1e3893f69a4c348e3ced
Георгий <voproshalo@yandex.ru>
2023-03-02 19:31:42 +0300

    Конвертер температуры

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Температура по Цельсию" + 32);
+        System.out.println(32 + " градуса по цельсию это " + (32 * 1.8 + 32));
+
+    }
+}
